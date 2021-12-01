b4659580d826df7f4e860568b658d754ce729f74
Neil Winton <nwinton@pivotal.io>
2021-12-01 09:50:05 +0000

    Just a bit of space

--- src/main/kotlin/aoc2021/Day01.kt
+++ src/main/kotlin/aoc2021/Day01.kt
@@ -2,5 +2,5 @@ package aoc2021
 
 object Day01 {
     fun numberOfIncreases(depths: List<Int>, window: Int = 1): Int =
-        depths.windowed(window).map { it.sum()}.zipWithNext().count { it.second > it.first }
+        depths.windowed(window).map { it.sum() }.zipWithNext().count { it.second > it.first }
 }
