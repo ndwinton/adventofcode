a75ad599e2936d1b336ac71949cb0eb466cd98d7
Neil Winton <nwinton@pivotal.io>
2021-12-12 19:19:25 +0000

    Day 12 - part 1 (simplified)

--- src/main/kotlin/aoc2021/Day12.kt
+++ src/main/kotlin/aoc2021/Day12.kt
@@ -5,16 +5,15 @@ fun findPaths(lines: List<String>): List<String> {
 
     fun findAllPaths(
         currentNode: String,
-        finished: List<List<String>>,
         inProgress: List<String>
     ): List<List<String>> {
-        return if (currentNode == "end") finished + listOf(inProgress + listOf("end"));
+        return if (currentNode == "end") listOf(inProgress + listOf("end"))
         else (connections[currentNode] ?: emptyList())
             .filter { node -> node[0].isUpperCase() || !inProgress.contains(node) }
-            .flatMap { findAllPaths(it, finished, inProgress + listOf(currentNode)) }
+            .flatMap { findAllPaths(it, inProgress + listOf(currentNode)) }
     }
 
-    return findAllPaths("start", emptyList(), emptyList()).map { it.joinToString(",") }
+    return findAllPaths("start", emptyList()).map { it.joinToString(",") }
 }
 
 fun buildConnections(lines: List<String>): Map<String,Set<String>> =
---
+++ src/test/kotlin/aoc2021/Day12Spec.kt
@@ -0,0 +1,74 @@
+package aoc2021
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.shouldBe
+
+class Day12Spec : FunSpec({
+    val example1 = """
+        start-A
+        start-b
+        A-c
+        A-b
+        b-d
+        A-end
+        b-end
+    """.trimIndent().lines()
+
+    val example2 = """
+        fs-end
+        he-DX
+        fs-he
+        start-DX
+        pj-DX
+        end-zg
+        zg-sl
+        zg-pj
+        pj-he
+        RW-he
+        fs-DX
+        pj-RW
+        zg-RW
+        start-pj
+        he-WI
+        zg-he
+        pj-fs
+        start-RW
+    """.trimIndent().lines()
+
+    val example1Paths = """
+        start,A,b,A,c,A,end
+        start,A,b,A,end
+        start,A,b,end
+        start,A,c,A,b,A,end
+        start,A,c,A,b,end
+        start,A,c,A,end
+        start,A,end
+        start,b,A,c,A,end
+        start,b,A,end
+        start,b,end
+    """.trimIndent().lines()
+
+    test("simplest example") {
+        findPaths(listOf("start-end")).shouldBe(listOf("start,end"))
+    }
+
+    test("build connections") {
+        buildConnections(example1).shouldBe(mapOf(
+            "start" to setOf("A", "b"),
+            "A" to setOf("start", "b", "c", "end"),
+            "b" to setOf("start", "A", "d", "end"),
+            "c" to setOf("A"),
+            "d" to setOf("b"),
+            "end" to setOf("A", "b")
+        ))
+    }
+
+    test("example paths") {
+        findPaths(example1).shouldContainExactlyInAnyOrder(example1Paths)
+    }
+
+    test("larger example") {
+        findPaths(example2).count().shouldBe(226)
+    }
+})
