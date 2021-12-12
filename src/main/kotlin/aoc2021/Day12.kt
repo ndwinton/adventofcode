4657eda1a1a13beb4d53a36606f6973060e528d4
Neil Winton <nwinton@pivotal.io>
2021-12-12 22:50:56 +0000

    Refactored to extract part 1 rule

--- src/main/kotlin/aoc2021/Day12.kt
+++ src/main/kotlin/aoc2021/Day12.kt
@@ -1,19 +1,20 @@
 package aoc2021
 
-fun findPaths(lines: List<String>): List<String> {
+fun findPaths(lines: List<String>, ruleFn: (String, List<String>) -> Boolean = ::part1Rule): List<String> {
     val connections = buildConnections(lines)
 
     fun findAllPaths(
         currentNode: String,
-        inProgress: List<String>
+        inProgress: List<String>,
+        ruleFn: (String, List<String>) -> Boolean
     ): List<List<String>> {
         return if (currentNode == "end") listOf(inProgress + listOf("end"))
         else (connections[currentNode] ?: emptyList())
-            .filter { node -> node[0].isUpperCase() || !inProgress.contains(node) }
-            .flatMap { findAllPaths(it, inProgress + listOf(currentNode)) }
+            .filter { ruleFn(it, inProgress) }
+            .flatMap { findAllPaths(it, inProgress + listOf(currentNode), ruleFn) }
     }
 
-    return findAllPaths("start", emptyList()).map { it.joinToString(",") }
+    return findAllPaths("start", emptyList(), ruleFn).map { it.joinToString(",") }
 }
 
 fun buildConnections(lines: List<String>): Map<String,Set<String>> =
@@ -21,3 +22,5 @@ fun buildConnections(lines: List<String>): Map<String,Set<String>> =
         .groupBy { it.first }
         .map { entry -> Pair(entry.key, entry.value.map { it.second }.toSet()) }
         .toMap()
+
+fun part1Rule(node: String, inProgress: List<String>): Boolean = node[0].isUpperCase() || !inProgress.contains(node)
