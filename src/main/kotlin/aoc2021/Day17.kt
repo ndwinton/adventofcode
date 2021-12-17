be8870661beb5b99fcde7034a568f9fe557e95c4
Neil Winton <nwinton@pivotal.io>
2021-12-17 13:18:28 +0000

    Day 17 - refactored with better names

--- src/main/kotlin/aoc2021/Day17.kt
+++ src/main/kotlin/aoc2021/Day17.kt
@@ -3,54 +3,61 @@ package aoc2021
 import kotlin.math.abs
 import kotlin.math.max
 
-fun xPositions(velocity: Int) = generateSequence(Pair(velocity, 1)) {
+data class PositionTime(val position: Int, val time: Int)
+
+fun xPositions(velocity: Int) = generateSequence(PositionTime(velocity, 1)) {
     // Pair first is x position, second is time
     when {
-        velocity > 0 && it.second < velocity -> Pair(it.first + (velocity - it.second), it.second + 1)
-        velocity < 0 && it.second < -velocity -> Pair(it.first + (velocity + it.second), it.second + 1)
-        else -> Pair(it.first, it.second + 1)
+        velocity > 0 && it.time < velocity -> PositionTime(it.position + (velocity - it.time), it.time + 1)
+        velocity < 0 && it.time < -velocity -> PositionTime(it.position + (velocity + it.time), it.time + 1)
+        else -> PositionTime(it.position, it.time + 1)
     }
 }
 
-fun yPositions(velocity: Int) = generateSequence(Pair(velocity, 1)) { Pair(it.first + velocity - it.second, it.second + 1) }
+fun yPositions(velocity: Int) = generateSequence(PositionTime(velocity, 1)) { PositionTime(it.position + velocity - it.time, it.time + 1) }
 
-fun minXToRange(low: Int, high: Int) = (1 .. high).dropWhile { (it * it + it) / 2 < low }.first()
+// Calculate minimum that can ever reach lower bound
+fun minForX(low: Int, high: Int) = (1 .. high).dropWhile { (it * it + it) / 2 < low }.first()
 
-// Pairs are (velocity, time)
-fun xInRange(low: Int, high: Int): List<Pair<Int,Int>> =
-    (minXToRange(low, high) .. high)
+data class VelocityTime(val velocity: Int, val time: Int)
+
+fun xInRange(low: Int, high: Int): List<VelocityTime> =
+    (minForX(low, high) .. high)
         .flatMap { velocity ->
             xPositions(velocity)
-                .takeWhile { it.first <= high && it.second <= 200 } // TODO: Work out better bounds
-                .dropWhile { it.first < low }
-                .map { Pair(velocity, it.second) } }
+                .takeWhile { it.position <= high && it.time <= 200 } // TODO: Work out better bounds
+                .dropWhile { it.position < low }
+                .map { VelocityTime(velocity, it.time) } }
 
-// Pairs are (velocity, time)
-fun yInRange(low: Int, high: Int): List<Pair<Int,Int>> =
-    (low .. max(abs(low), abs(high)))
+// Max for Y - under gravity, if travelling faster than the maximum depth of
+// the target per tick at y = 0, it will always overshoot
+fun maxForY(low: Int, high: Int) = max(abs(low), abs(high))
+
+fun yInRange(low: Int, high: Int): List<VelocityTime> =
+    (low .. maxForY(low, high))
         .flatMap { velocity ->
             yPositions(velocity)
-                .dropWhile { it.first > high }
-                .takeWhile { it.first >= low && it.second <= 200 } // TODO: Work out better bounds
-                .map { Pair(velocity, it.second) } }
+                .dropWhile { it.position > high }
+                .takeWhile { it.position >= low && it.time <= 200 } // TODO: Work out better bounds
+                .map { VelocityTime(velocity, it.time) } }
 
-fun findMaxYVelocity(xPos: List<Pair<Int,Int>>, yPos: List<Pair<Int,Int>>): Int {
-    val xByTime = xPos.groupBy { it.second }
-    val yByTime = yPos.groupBy { it.second }
+fun findMaxYVelocity(xPos: List<VelocityTime>, yPos: List<VelocityTime>): Int {
+    val xByTime = xPos.groupBy { it.time }
+    val yByTime = yPos.groupBy { it.time }
 
-    return yByTime.filter { xByTime.containsKey(it.key) }.map { (_, list) -> list.maxOf { it.first }}.maxOf { it }
+    return yByTime.filter { xByTime.containsKey(it.key) }.map { (_, list) -> list.maxOf { it.velocity }}.maxOf { it }
 }
 
 fun findMaximumHeight(xLow: Int, xHigh: Int, yLow: Int, yHigh: Int) =
-    yPositions(findMaxYVelocity(xInRange(xLow, xHigh), yInRange(yLow, yHigh))).takeWhile { it.first > 0 }.maxOf { it.first }
+    yPositions(findMaxYVelocity(xInRange(xLow, xHigh), yInRange(yLow, yHigh))).takeWhile { it.position > 0 }.maxOf { it.position }
 
 fun totalDistinctVelocities(xLow: Int, xHigh: Int, yLow: Int, yHigh: Int): Int {
-    val xByTime = xInRange(xLow, xHigh).groupBy { it.second }
-    val yByTime = yInRange(yLow, yHigh).groupBy { it.second }
+    val xByTime = xInRange(xLow, xHigh).groupBy { it.time }
+    val yByTime = yInRange(yLow, yHigh).groupBy { it.time }
 
     return yByTime.filter { xByTime.containsKey(it.key) }
         .flatMap { (time, yList) ->
-            yList.map { it.first }.cartesianProduct((xByTime[time] ?: emptyList()).map { it.first })
+            yList.map { it.velocity }.cartesianProduct((xByTime[time] ?: emptyList()).map { it.velocity })
         }
         .distinct().size
 }
--- src/test/kotlin/aoc2021/Day17Spec.kt
+++ src/test/kotlin/aoc2021/Day17Spec.kt
@@ -1,7 +1,6 @@
 package aoc2021
 
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
 import io.kotest.matchers.collections.shouldNotContainAll
@@ -10,31 +9,31 @@ import io.kotest.matchers.shouldBe
 class Day17Spec : FunSpec({
     test("x position sequence") {
         xPositions(3).take(5).toList().shouldBe(listOf(
-            Pair(3, 1), Pair(5, 2), Pair(6, 3), Pair(6, 4), Pair(6, 5)
+            PositionTime(3, 1), PositionTime(5, 2), PositionTime(6, 3), PositionTime(6, 4), PositionTime(6, 5)
         ))
 
         xPositions(-3).take(5).toList().shouldBe(listOf(
-            Pair(-3, 1), Pair(-5, 2), Pair(-6, 3), Pair(-6, 4), Pair(-6, 5)
+            PositionTime(-3, 1), PositionTime(-5, 2), PositionTime(-6, 3), PositionTime(-6, 4), PositionTime(-6, 5)
         ))
     }
 
     test("y position sequence") {
         yPositions(3).take(7).toList().shouldBe(listOf(
-            Pair(3, 1), Pair(5, 2), Pair(6, 3), Pair(6, 4), Pair(5, 5), Pair(3, 6), Pair(0, 7)
+            PositionTime(3, 1), PositionTime(5, 2), PositionTime(6, 3), PositionTime(6, 4), PositionTime(5, 5), PositionTime(3, 6), PositionTime(0, 7)
         ))
     }
 
     test("x in range") {
-        xInRange(20, 30).shouldContainAll(Pair(6, 5), Pair(6, 30), Pair(7, 4), Pair(9, 3))
-        xInRange(20, 30).shouldNotContainAll(Pair(6, 4), Pair(6, 31), Pair(7, 3), Pair(9, 4))
+        xInRange(20, 30).shouldContainAll(VelocityTime(6, 5), VelocityTime(6, 30), VelocityTime(7, 4), VelocityTime(9, 3))
+        xInRange(20, 30).shouldNotContainAll(VelocityTime(6, 4), VelocityTime(6, 31), VelocityTime(7, 3), VelocityTime(9, 4))
     }
 
     test("y in range") {
         yInRange(-10, -5).shouldContainExactlyInAnyOrder(
-            Pair(-10, 1), Pair(-9, 1), Pair(-8, 1), Pair(-7, 1), Pair(-6, 1), Pair(-5, 1),
-            Pair(-4, 2), Pair(-3, 2), Pair(-2, 2), Pair(-2, 3), Pair(-1, 3), Pair(-1, 4),
-            Pair(0, 4), Pair(0, 5), Pair(1, 5), Pair(1, 6), Pair(2, 7), Pair(3, 9), Pair(4, 10),
-            Pair(5, 12), Pair(6, 14), Pair(7, 16), Pair(8, 18), Pair(9, 20)
+            VelocityTime(-10, 1), VelocityTime(-9, 1), VelocityTime(-8, 1), VelocityTime(-7, 1), VelocityTime(-6, 1), VelocityTime(-5, 1),
+            VelocityTime(-4, 2), VelocityTime(-3, 2), VelocityTime(-2, 2), VelocityTime(-2, 3), VelocityTime(-1, 3), VelocityTime(-1, 4),
+            VelocityTime(0, 4), VelocityTime(0, 5), VelocityTime(1, 5), VelocityTime(1, 6), VelocityTime(2, 7), VelocityTime(3, 9), VelocityTime(4, 10),
+            VelocityTime(5, 12), VelocityTime(6, 14), VelocityTime(7, 16), VelocityTime(8, 18), VelocityTime(9, 20)
         )
     }
 
