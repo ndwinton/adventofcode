d77307d8733b4b72a261cd41adaa320354a6fb08
Neil Winton <neil@winton.org.uk>
2024-12-02 23:01:04 +0000

    2024 - Day 2 - more minor tweaks

--- src/main/kotlin/aoc2024/Day2402.kt
+++ src/main/kotlin/aoc2024/Day2402.kt
@@ -4,13 +4,12 @@ fun isLevelSafe(level: List<Int>) : Boolean =
     level.zipWithNext()
         .map { it.first - it.second }
         .let { diffs ->
-            diffs.all { it in -3 .. -1 } || diffs.all { it in 1..3 }
+            diffs.all { it in -3..-1 } || diffs.all { it in 1..3 }
         }
 
-tailrec fun levelDampened(level: List<Int>, index: Int = 0, result : List<List<Int>> = listOf(level)) : List<List<Int>> {
-    return if (index >= level.size) result
+tailrec fun levelDampened(level: List<Int>, index: Int = 0, result : List<List<Int>> = listOf(level)) : List<List<Int>> =
+    if (index >= level.size) result
     else levelDampened(level, index + 1, result + listOf(level.take(index) + level.drop(index + 1)))
-}
 
 fun isDampenedLevelSafe(level: List<Int>) =
     levelDampened(level).any { isLevelSafe(it) }
