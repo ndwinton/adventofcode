c8c754ad56039aaecc0e78d517c28482f7c1a424
Neil Winton <neil@winton.org.uk>
2025-12-01 20:25:02 +0000

    2025 - Day 1 - refactored for clarity

--- src/main/kotlin/aoc2025/Day2501.kt
+++ src/main/kotlin/aoc2025/Day2501.kt
@@ -3,26 +3,27 @@ package aoc2025
 import kotlin.math.absoluteValue
 import kotlin.math.sign
 
+private data class State(val position: Int, val zeroes: Int)
+
 fun numberOfStopsOnZero(input: List<String>) =
     input.map { it.drop(1).toInt() * (if (it.first() == 'L') -1 else 1) }
-        .fold(Pair(50, 0)) { state, next ->
-            // println("$state, $next")
-            val newPosition = state.first + next
-            val newZeroes = state.second + (if (newPosition % 100 == 0) 1 else 0)
-            Pair(newPosition, newZeroes)
-        }.second
+        .fold(State(50, 0)) { state, move ->
+            val newPosition = state.position + move
+            val newZeroes = state.zeroes + (if (newPosition % 100 == 0) 1 else 0)
+            State(newPosition, newZeroes)
+        }.zeroes
 
 fun numberOfPassesThroughZero(input: List<String>) =
     input.map { it.drop(1).toInt() * (if (it.first() == 'L') -1 else 1) }
-        .fold(Pair(50, 0)) { state, next ->
-            val completeTurns = (next / 100).absoluteValue
-            val remainder = next % 100
-            val newPosition = state.first + remainder
+        .fold(State(50, 0)) { state, move ->
+            val completeTurns = (move / 100).absoluteValue
+            val remainder = move % 100
+            val newPosition = state.position + remainder
             val passedOrStoppedOnZero = when {
                 newPosition.absoluteValue > 100 -> 1
-                newPosition.sign == -state.first.sign -> 1
+                newPosition.sign == -state.position.sign -> 1
                 newPosition % 100 == 0  -> 1
                 else -> 0
             }
-            Pair(newPosition % 100, state.second + completeTurns + passedOrStoppedOnZero)
-        }.second
+            State(newPosition % 100, state.zeroes + completeTurns + passedOrStoppedOnZero)
+        }.zeroes
