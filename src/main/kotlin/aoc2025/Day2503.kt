ca4b2b9d7b89dbbebaa2d8a7d17c4420e6ee4b83
Neil Winton <neil@winton.org.uk>
2025-12-03 09:17:36 +0000

    2025 - Day 3 - part 2 (simplified)

--- src/main/kotlin/aoc2025/Day2503.kt
+++ src/main/kotlin/aoc2025/Day2503.kt
@@ -3,12 +3,9 @@ package aoc2025
 fun joltageOfBank(bank: String, size: Int = 2) : Long =
     bank.dropLast(size - 1)
         .max()
-        .let { first ->
-            if (size == 2) {
-                (first + "" + bank.substring(bank.indexOf(first) + 1).max()).toLong()
-            } else {
-                (first + "" + joltageOfBank(bank.substring(bank.indexOf(first) + 1), size - 1)).toLong()
-            }
-        }
+        .let { highest ->
+            val remaining = bank.substring(bank.indexOf(highest) + 1)
+            highest + "" + if (size == 2) remaining.max() else joltageOfBank(remaining, size - 1)
+        }.toLong()
 
 fun totalJoltageOfBanks(banks: List<String>, size: Int = 2) = banks.sumOf { joltageOfBank(it, size) }
